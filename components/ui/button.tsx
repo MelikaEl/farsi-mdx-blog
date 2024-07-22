239ad21bb3ef3c8f1f87dd5f469bd91a6d0ae660
MelikaEl <melika.melika8@gmail.com>
2024-07-22 10:53:45 +0430

    change-hover-color-on-button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-primary text-primary-foreground hover:bg-[#047a7a]",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
