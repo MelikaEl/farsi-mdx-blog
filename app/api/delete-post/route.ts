6ccc599b0f020ff6fd73c2da571b712bdd351884
MelikaEl <melika.melika8@gmail.com>
2024-07-23 13:07:04 +0430

    correct-functionality-of-delete-post-button

--- app/api/delete-post/route.ts
+++ app/api/delete-post/route.ts
@@ -4,33 +4,75 @@ export async function POST(req: Request) {
   if (req.method === "POST") {
     try {
       const data = await req.json();
-
-      deleteLocalFile(data);
+      await deleteLocalFile(data); // Await the async function
+      return new Response(
+        JSON.stringify({ message: "Post deleted successfully" }),
+        {
+          status: 200,
+          headers: { "content-type": "application/json" },
+        }
+      );
     } catch (error) {
       console.error("Error:", error);
       return new Response(
-        JSON.stringify({
-          message: "Error in processing your request",
-        }),
+        JSON.stringify({ message: "Error in processing your request" }),
         {
           status: 500,
-          headers: {
-            "content-type": "application/json",
-          },
+          headers: { "content-type": "application/json" },
         }
       );
     }
   } else {
-    // Handle any non-POST requests
     return new Response(
-      JSON.stringify({
-        message: "This endpoint only accepts POST requests",
-      }),
+      JSON.stringify({ message: "This endpoint only accepts POST requests" }),
       {
-        headers: {
-          "content-type": "application/json",
-        },
+        headers: { "content-type": "application/json" },
       }
     );
   }
 }
+
+
+
+
+
+
+
+
+
+// import { deleteLocalFile } from "@/lib/delete-post";
+
+// export async function POST(req: Request) {
+//   if (req.method === "POST") {
+//     try {
+//       const data = await req.json();
+
+//       deleteLocalFile(data);
+//     } catch (error) {
+//       console.error("Error:", error);
+//       return new Response(
+//         JSON.stringify({
+//           message: "Error in processing your request",
+//         }),
+//         {
+//           status: 500,
+//           headers: {
+//             "content-type": "application/json",
+//           },
+//         }
+//       );
+//     }
+//   } else {
+//     // Handle any non-POST requests
+//     return new Response(
+//       JSON.stringify({
+//         message: "This endpoint only accepts POST requests",
+//       }),
+//       {
+//         headers: {
+//           "content-type": "application/json",
+//         },
+//       }
+//     );
+//   }
+// }
