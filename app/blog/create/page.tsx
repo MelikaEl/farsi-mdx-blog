9ed1ad4d48388889dffb4296e83edbbe1d12c12a
MelikaEl <melika.melika8@gmail.com>
2024-08-20 10:13:36 +0430

    vercel-error-exchange-repository-code

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,17 +1,37 @@
-"use client"
-import { useState } from "react";
 import { CreatePostForm } from "./create-post-form";
 
 export default function Admin() {
-  const [markdown, setMarkdown] = useState<string>("");
   return (
     <div className="w-full max-w-xl">
       <div className="flex flex-col gap-10">
         <h1 className="text-4xl sm:text-5xl font-bold text-center">
-        ایجاد یک پست
+          Create a Post
         </h1>
-        <CreatePostForm markdown={markdown}/>
+        <CreatePostForm />
       </div>
     </div>
   );
 }
+
+
+
+
+
+//correct code
+// "use client"
+// import { useState } from "react";
+// import { CreatePostForm } from "./create-post-form";
+
+// export default function Admin() {
+//   const [markdown, setMarkdown] = useState<string>("");
+//   return (
+//     <div className="w-full max-w-xl">
+//       <div className="flex flex-col gap-10">
+//         <h1 className="text-4xl sm:text-5xl font-bold text-center">
+//         ایجاد یک پست
+//         </h1>
+//         <CreatePostForm markdown={markdown}/>
+//       </div>
+//     </div>
+//   );
+// }
