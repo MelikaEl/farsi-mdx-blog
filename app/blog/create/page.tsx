03bbdf6550590ae52c0c36d3ae3c1c5728df30aa
MelikaEl <melika.melika8@gmail.com>
2024-08-24 14:19:23 +0430

    add-markdown-prop-to-create-post-form

--- app/blog/create/page.tsx
+++ app/blog/create/page.tsx
@@ -1,13 +1,16 @@
+"use client"
+import { useState } from "react";
 import { CreatePostForm } from "./create-post-form";
 
 export default function Admin() {
+  const [markdown, setMarkdown] = useState<string>("");
   return (
     <div className="w-full max-w-xl">
       <div className="flex flex-col gap-10">
         <h1 className="text-4xl sm:text-5xl font-bold text-center">
-          Create a Post
+         ایجاد پست
         </h1>
-        <CreatePostForm />
+        <CreatePostForm markdown={markdown}/>
       </div>
     </div>
   );
